3ed8c1a3c3fab33391a29db464f9912079920bea
hoshi524 <hoshi524@gmail.com>
2017-08-17 22:58:29 +0900

    update

--- src/ConnectedComponent.cpp
+++ src/ConnectedComponent.cpp
@@ -4,10 +4,8 @@ using namespace std;
 constexpr double TIME_LIMIT = 2000;
 constexpr int MAX_S = 500;
 
-double START_TIME;
 int S;
 int M[MAX_S][MAX_S];
-int C[MAX_S][MAX_S];
 
 double get_time() {
   unsigned long long a, d;
@@ -20,10 +18,12 @@ unsigned get_random() {
   return y ^= (y ^= (y ^= y << 13) >> 17) << 5;
 }
 
+double get_random_double() { return (double)get_random() / UINT_MAX; }
+
 class ConnectedComponent {
  public:
   vector<int> permute(vector<int> matrix) {
-    START_TIME = get_time();
+    double START_TIME = get_time();
     S = (int)sqrt(matrix.size());
     for (int i = 0; i < S; ++i) {
       for (int j = 0; j < S; ++j) {
@@ -31,69 +31,75 @@ class ConnectedComponent {
       }
     }
     vector<int> ret(S);
-    vector<int> per(S);
     double score = 0;
-    int remain[MAX_S];
     for (int i = 0; i < S; ++i) {
-      remain[i] = i;
+      ret[i] = i;
     }
-    while ((START_TIME + TIME_LIMIT - get_time()) / TIME_LIMIT > 0) {
-      for (int i = 0; i < S; ++i) {
-        int t = get_random() % (S - i);
-        per[i] = remain[t];
-        int v = remain[t];
-        remain[t] = remain[S - i - 1];
-        remain[S - i - 1] = v;
-      }
-      for (int i = 0; i < S; ++i)
-        for (int j = 0; j < S; ++j) C[i][j] = M[per[i]][per[j]];
-      double s = calc();
+    while (true) {
+      double time = (START_TIME + TIME_LIMIT - get_time()) / TIME_LIMIT;
+      if (time < 0) break;
+      int a = get_random() % S;
+      int b = get_random() % S;
+      if (a == b) continue;
+      swap(ret, a, b);
+      double s = calc(ret);
       if (score < s) {
         score = s;
-        ret = per;
+      } else {
+        swap(ret, a, b);
       }
     }
     return ret;
   }
 
-  double calc() {
+  void swap(vector<int> &v, int a, int b) {
+    int t = v[a];
+    v[a] = v[b];
+    v[b] = t;
+  }
+
+  double calc(vector<int> &v) {
+    int C[MAX_S][MAX_S];
+    for (int i = 0; i < S; ++i)
+      for (int j = 0; j < S; ++j) C[i][j] = M[v[i]][v[j]];
+
     double score = 0;
-    bool ok[MAX_S][MAX_S];
-    memset(ok, false, sizeof(ok));
     int qx[MAX_S * MAX_S];
     int qy[MAX_S * MAX_S];
     for (int i = 0; i < S; ++i) {
       for (int j = 0; j < S; ++j) {
-        if (C[i][j] == 0 || ok[i][j]) continue;
-        ok[i][j] = true;
-        int qi = 0, qs = 1, sum = 0;
+        if (C[i][j] == 0) continue;
+        int qi = 0, qs = 1, sum = C[i][j];
         qx[qi] = i;
         qy[qi] = j;
         while (qi < qs) {
           int x = qx[qi];
           int y = qy[qi];
           ++qi;
-          sum += C[x][y];
-          if (x + 1 < S && C[x + 1][y] != 0 && !ok[x + 1][y]) {
-            ok[x + 1][y] = true;
+          if (x + 1 < S && C[x + 1][y]) {
+            sum += C[x + 1][y];
+            C[x + 1][y] = 0;
             qx[qs] = x + 1;
             qy[qs] = y;
             ++qs;
           }
-          if (x - 1 >= 0 && C[x - 1][y] != 0 && !ok[x - 1][y]) {
-            ok[x - 1][y] = true;
+          if (x - 1 >= 0 && C[x - 1][y]) {
+            sum += C[x - 1][y];
+            C[x - 1][y] = 0;
             qx[qs] = x - 1;
             qy[qs] = y;
             ++qs;
           }
-          if (y + 1 < S && C[x][y + 1] != 0 && !ok[x][y + 1]) {
-            ok[x][y + 1] = true;
+          if (y + 1 < S && C[x][y + 1]) {
+            sum += C[x][y + 1];
+            C[x][y + 1] = 0;
             qx[qs] = x;
             qy[qs] = y + 1;
             ++qs;
           }
-          if (y - 1 >= 0 && C[x][y - 1] != 0 && !ok[x][y - 1]) {
-            ok[x][y - 1] = true;
+          if (y - 1 >= 0 && C[x][y - 1]) {
+            sum += C[x][y - 1];
+            C[x][y - 1] = 0;
             qx[qs] = x;
             qy[qs] = y - 1;
             ++qs;
