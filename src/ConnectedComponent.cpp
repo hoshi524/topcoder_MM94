f6e47c6faf31c7a61b7a968b80214da0852aeac5
hoshi524 <hoshi524@gmail.com>
2017-08-18 12:32:21 +0900

    update

--- src/ConnectedComponent.cpp
+++ src/ConnectedComponent.cpp
@@ -2,11 +2,10 @@
 using namespace std;
 
 constexpr double TIME_LIMIT = 3000;
-constexpr int MAX_S = 500;
+constexpr int MAX_S = 1 << 9;
 
 int S;
 int M[MAX_S][MAX_S];
-int C[MAX_S + 2][MAX_S + 2];
 
 double get_time() {
   unsigned long long a, d;
@@ -21,23 +20,34 @@ unsigned get_random() {
 
 double get_random_double() { return (double)get_random() / UINT_MAX; }
 
+void swap(vector<int> &v, int a, int b) {
+  int t = v[a];
+  v[a] = v[b];
+  v[b] = t;
+}
+
+int to(int x, int y) { return (x << 9) + y; }
+
 class ConnectedComponent {
  public:
   vector<int> permute(vector<int> matrix) {
     double START_TIME = get_time();
     S = (int)sqrt(matrix.size());
-    memset(C, 0, sizeof(C));
     for (int i = 0; i < S; ++i) {
       for (int j = 0; j < S; ++j) {
         M[i][j] = matrix[i * S + j];
       }
     }
+
     vector<int> ret(S);
     double score = 0;
     for (int i = 0; i < S; ++i) {
       ret[i] = i;
     }
+
     int queue[MAX_S * MAX_S];
+    int C[MAX_S * MAX_S];
+    memset(C, 0, sizeof(C));
     for (int iter = 0, prev = 0; true; ++iter) {
       double time = (START_TIME + TIME_LIMIT - get_time()) / TIME_LIMIT;
       if (time < 0) break;
@@ -47,38 +57,35 @@ class ConnectedComponent {
       swap(ret, a, b);
 
       for (int i = 0; i < S; ++i)
-        for (int j = 0; j < S; ++j) C[i + 1][j + 1] = M[ret[i]][ret[j]];
+        for (int j = 0; j < S; ++j) C[to(i + 1, j + 1)] = M[ret[i]][ret[j]];
       double s = 0;
-      auto search = [&](int x, int y) {
-        if (C[x][y] == 0) return;
-        int qi = 0, qs = 2, sum = C[x][y];
-        C[x][y] = 0;
-        queue[0] = x;
-        queue[1] = y;
-        auto push = [&](int x, int y) {
-          if (C[x][y]) {
-            sum += C[x][y];
-            C[x][y] = 0;
-            queue[qs++] = x;
-            queue[qs++] = y;
+      auto search = [&](int p) {
+        if (C[p] == 0) return;
+        int qi = 0, qs = 1, sum = C[p];
+        C[p] = 0;
+        queue[0] = p;
+        auto push = [&](int p) {
+          if (C[p]) {
+            sum += C[p];
+            C[p] = 0;
+            queue[qs++] = p;
           }
         };
         while (qi < qs) {
-          x = queue[qi++];
-          y = queue[qi++];
-          push(x + 1, y);
-          push(x - 1, y);
-          push(x, y + 1);
-          push(x, y - 1);
+          p = queue[qi++];
+          push(p + 1);
+          push(p - 1);
+          push(p + MAX_S);
+          push(p - MAX_S);
         }
-        double t = sum * sqrt(qs >> 1);
+        double t = sum * sqrt(qs);
         if (s < t) s = t;
       };
       for (int i = 1; i <= S; ++i) {
-        search(i, a + 1);
-        search(i, b + 1);
-        search(a + 1, i);
-        search(b + 1, i);
+        search(to(i, a + 1));
+        search(to(i, b + 1));
+        search(to(a + 1, i));
+        search(to(b + 1, i));
       }
 
       if (score <
@@ -91,12 +98,6 @@ class ConnectedComponent {
       // cerr << score << endl;
     }
     return ret;
-  }
-
-  void swap(vector<int> &v, int a, int b) {
-    int t = v[a];
-    v[a] = v[b];
-    v[b] = t;
   }
 };
 
